99831cabd38d55adabdf4e3e31e550b389058703
Dé Limuscle <darkcrecerelle@gmail.com>
2025-08-24 18:03:34 +0200

    pix perfect game

---
+++ Tests/src/PixPerfectGame.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <SandCastle.h>
+using namespace SandCastle;
+
+struct Swordman
+{
+	int t;
+};
+class SwordmanSys : public System
+{
+	void Start() override
+	{
+		auto e = Entity::CreateAnimatedSprite("swordman_walk.anim");
+		e.adc<Swordman>();
+		Inputs::Get("Player", "Dest")->signal.Listen(&SwordmanSys::OnDest, this);
+	}
+	void MoveToDest(Transform& tr)
+	{
+		auto dir = (dest - tr.GetPosition()).Normalized();
+		auto delta = Time::Delta();
+		auto offset = dir * speed * delta;
+
+		tr.Move(offset);
+	}
+	void FlipSprite(Transform& tr)
+	{
+		if (dest.x > tr.GetPosition().x)
+			tr.SetScale(-1, 1, 1);
+		else
+			tr.SetScale(1, 1, 1);
+	}
+	void Update() override
+	{
+		auto delta = Time::Delta();
+		auto view = Entity::View<Swordman, Transform>();
+		view.each([&](Swordman& t, Transform& tr)
+			{
+				MoveToDest(tr);
+			});
+	}
+	void OnDest(InputSignal* signal)
+	{
+		dest = Mouse::GetWorldPos();
+		auto view = Entity::View<Swordman, Transform>();
+		view.each([&](Swordman& t, Transform& tr)
+			{
+				FlipSprite(tr);
+			});
+	}
+	Vec3f dest;
+	float speed = 20.f;
+};
+void PixPerfectGame()
+{
+	Engine::Init();
+	Window::SetClearColor(Vec4f(0.6, 0.6, 0.6, 1.f));
+	Camera::main->zoom = 0.002777777f; //pixel perfect for 1.f ppu texture
+	auto inputs = Inputs::CreateInputMap("Player");
+	auto dir = inputs->CreateButtonInput("Dest");
+	dir->BindMouse(Mouse::Button::Left);
+	Systems::Push<SwordmanSys>();
+	Engine::Launch();
+}
--- Tests/src/main.cpp
+++ Tests/src/main.cpp
@@ -11,47 +11,9 @@
 #include "Signals.h"
 #include "FontTest.h"
 #include "LayerTest.h"
+#include "PixPerfectGame.h"
 
 using namespace SandCastle;
-
-
-struct Tag
-{
-	int t;
-};
-class MoveS : public System
-{
-	void Start() override
-	{
-		auto e = Entity::CreateAnimatedSprite("swordman_walk.anim");
-		e.adc<Tag>();
-
-		Inputs::Get("Player", "Dest")->signal.Listen(&MoveS::OnDest, this);
-	}
-	void MoveToDest(Transform& tr)
-	{
-		auto dir = (dest - tr.GetPosition()).Normalized();
-		auto delta = Time::Delta();
-		auto offset = dir * speed * delta;
-
-		tr.Move(offset);
-	}
-	void Update() override
-	{
-		auto delta = Time::Delta();
-		auto view = Entity::View<Tag, Transform>();
-		view.each([&](Tag& t, Transform& tr)
-			{
-				MoveToDest(tr);
-			});
-	}
-	void OnDest(InputSignal* signal)
-	{
-		dest = Mouse::GetWorldPos();
-	}
-	Vec3f dest;
-	float speed = 5.f;
-};
 int main()
 {
 	//Launch();
@@ -66,13 +28,6 @@ int main()
 	//Signals();
 	//FontTest();
 	//LayerTest();
+	PixPerfectGame();
 
-	Engine::Init();
-	Window::SetClearColor(Vec4f(0.6, 0.6, 0.6, 1.f));
-	Camera::main->zoom = 0.002777777f; //pixel perfect for 1.f ppu texture
-	auto inputs = Inputs::CreateInputMap("Player");
-	auto dir = inputs->CreateButtonInput("Dest");
-	dir->BindMouse(Mouse::Button::Left);
-	Systems::Push<MoveS>();
-	Engine::Launch();
 }
