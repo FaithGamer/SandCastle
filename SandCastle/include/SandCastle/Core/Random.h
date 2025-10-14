ae5fbc8396a64c068c00e9d180007dea23d83c65
Dé Limuscle <darkcrecerelle@gmail.com>
2025-10-14 22:04:00 +0200

    random pick from vector

--- SandCastle/include/SandCastle/Core/Random.h
+++ SandCastle/include/SandCastle/Core/Random.h
@@ -9,5 +9,20 @@ namespace SandCastle
 		uint64_t Range(uint64_t min, uint64_t max);
 		int64_t Range(int64_t min, int64_t max);
 		float Range(float min, float max);
+		template<typename T>
+		T PickAndRemove(std::vector<T>& container)
+		{
+			int64_t r = Range((int64_t)0, (int64_t)(container.size() - 1));
+			T ret = container[r];
+			container[r] = container.back();
+			container.pop_back();
+			return ret;
+		}
+		template<typename T>
+		T Pick(const std::vector<T>& container)
+		{
+			int64_t r = Range((int64_t)0, (int64_t)(container.size() - 1));
+			return container[r];
+		}
 	}
 }
