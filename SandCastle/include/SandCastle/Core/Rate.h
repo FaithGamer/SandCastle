7510e4d296d8d086bd64892e01a0848128894036
Dé Limuscle <darkcrecerelle@gmail.com>
2026-04-24 11:30:56 +0200

    improved rate

--- SandCastle/include/SandCastle/Core/Rate.h
+++ SandCastle/include/SandCastle/Core/Rate.h
@@ -9,15 +9,18 @@ namespace SandCastle
 	{
 	public:
 		Rate() = default;
-		Rate(double period, size_t sampleMax = 100);
+		Rate(double period, size_t sampleMax = 100, double autoMaxPeriod = 0., double rateUpdatePeriod = 0.);
 
 		void Tick(double amount);
 		void Update(float delta);
 		void SetSampleMax(size_t max);
+		void SetAutoMaxPeriod(double period);
+		void SetRateUpdatePeriod(double period);
 		double rate = 0.;
 		double minRate = 1.;
 
 	private:
+		void Compute();
 		void PushSample(double sample);
 		double m_lastInterval = 0.;
 		double m_period = 1.;
@@ -27,5 +30,11 @@ namespace SandCastle
 		std::deque<double> m_samples;
 		double m_sampleSum = 0.;
 		size_t m_sampleMax = 100;
+		double m_autoMaxPeriod = 0.;
+		double m_autoMaxTimer = 0.;
+		size_t m_autoMaxCount = 0;
+		double m_rateUpdatePeriod = 0.;
+		double m_rateUpdateTimer = 0.;
+		bool m_rateDirty = false;
 	};
 }
--- SandCastle/src/Core/Rate.cpp
+++ SandCastle/src/Core/Rate.cpp
@@ -1,11 +1,13 @@
 #include "pch.h"
 #include "SandCastle/Core/Rate.h"
-
+#define ABSOLUTE_MAX 10000
 namespace SandCastle
 {
-	Rate::Rate(double period, size_t sampleMax) :
+	Rate::Rate(double period, size_t sampleMax, double autoMaxPeriod, double rateUpdatePeriod) :
 		m_period(period),
-		m_sampleMax(sampleMax)
+		m_sampleMax(sampleMax),
+		m_autoMaxPeriod(autoMaxPeriod),
+		m_rateUpdatePeriod(rateUpdatePeriod)
 	{
 	}
 
@@ -17,6 +19,26 @@ namespace SandCastle
 	{
 		m_time += (double)delta;
 		m_decrTime += (double)delta;
+		if (m_autoMaxPeriod > 0.)
+		{
+			m_autoMaxTimer += (double)delta;
+			if (m_autoMaxTimer >= m_autoMaxPeriod)
+			{
+				size_t newMax = m_autoMaxCount > 0 ? m_autoMaxCount : 1;
+				newMax = std::min(newMax, (size_t)ABSOLUTE_MAX);
+				if (newMax != m_sampleMax)
+				{
+					m_sampleMax = newMax;
+					while (m_samples.size() > m_sampleMax)
+					{
+						m_sampleSum -= m_samples.front();
+						m_samples.pop_front();
+					}
+				}
+				m_autoMaxCount = 0;
+				m_autoMaxTimer = 0.;
+			}
+		}
 		if (m_ticks > 0)
 		{
 			PushSample(m_ticks / m_time * m_period);
@@ -30,6 +52,18 @@ namespace SandCastle
 			PushSample(0.);
 			m_decrTime = 0.;
 		}
+		if (m_rateUpdatePeriod > 0.)
+		{
+			m_rateUpdateTimer += (double)delta;
+			if (m_rateUpdateTimer >= m_rateUpdatePeriod)
+			{
+				if (m_rateDirty)
+				{
+					Compute();
+				}
+				m_rateUpdateTimer = 0.;
+			}
+		}
 	}
 	void Rate::SetSampleMax(size_t max)
 	{
@@ -37,20 +71,44 @@ namespace SandCastle
 		m_sampleSum = 0.;
 		m_sampleMax = max;
 	}
+	void Rate::SetAutoMaxPeriod(double period)
+	{
+		m_autoMaxPeriod = period;
+		m_autoMaxTimer = 0.;
+		m_autoMaxCount = 0;
+	}
+	void Rate::SetRateUpdatePeriod(double period)
+	{
+		m_rateUpdatePeriod = period;
+		m_rateUpdateTimer = 0.;
+	}
+
+	void Rate::Compute()
+	{
+		if (m_sampleSum > 0. && m_samples.size() > 0)
+			rate = m_sampleSum / (double)m_samples.size();
+		else
+			rate = 0;
+		m_rateDirty = false;
+	}
 
 	void Rate::PushSample(double sample)
 	{
 		m_samples.push_back(sample);
 		m_sampleSum += sample;
+		++m_autoMaxCount;
 		if (m_samples.size() > m_sampleMax)
 		{
 			m_sampleSum -= m_samples.front();
 			m_samples.pop_front();
 		}
-		if (m_sampleSum > 0. && m_samples.size() > 0)
-			rate = m_sampleSum / (double)m_samples.size();
+		if (m_rateUpdatePeriod > 0.)
+		{
+			m_rateDirty = true;
+		}
 		else
-			rate = 0;
-
+		{
+			Compute();
+		}
 	}
 }
